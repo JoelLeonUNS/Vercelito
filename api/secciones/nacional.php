cfcf2efd4518106e119f3427a13d5bc10b0ee892
Joel Leon M <109642410+JoelLeonUNS@users.noreply.github.com>
2025-06-11 10:17:50 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Foro Industrial 2025: UNMSM y SNI unen esfuerzos por una industria nacional que genere empleo",
+"autor" => "UNMSM",
+"resumen" => "La UNMSM y la SNI organizarán el Foro Industrial 2025 como parte de la Semana de la Industria, promoviendo el diálogo entre universidad e industria para fomentar empleo y desarrollo económico. El evento reunirá a académicos, empresarios y especialistas el 9 de junio en la Biblioteca Central de la UNMSM.
+",
 ];
 ?>
