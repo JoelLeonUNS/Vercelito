eff97ae39f468f7a0aae313326fb3aa4712900ae
Joel Leon M <109642410+JoelLeonUNS@users.noreply.github.com>
2025-06-11 10:18:31 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -6,6 +6,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $internacional = [
 "titulo" => "Builder.ai, el increíble caso de la startup que prometió IA y entregó trabajo manual",
 "autor" => "Entrepreneur",
-"resumen" => "La empresa ocultó el rol de más de 700 ingenieros y simuló operaciones con empresas asociadas, como Microsoft.",
+"resumen" => "Builder.ai, startup fundada en 2016 y respaldada por Microsoft, prometía desarrollo de software automatizado con IA, pero operaba en realidad con cientos de ingenieros humanos. Tras incumplir un préstamo y revelarse prácticas financieras irregulares, la empresa entró en insolvencia, resaltando la necesidad de mayor transparencia en el sector tecnológico.
+",
 ];
 ?>
