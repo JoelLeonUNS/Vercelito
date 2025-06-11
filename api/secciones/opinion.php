08bfc280b65ac94b9d961069bb54f13b8d22d35c
Joel Leon M <109642410+JoelLeonUNS@users.noreply.github.com>
2025-06-11 10:23:02 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Liquid Glass de iOS 26 es espectacular, pero no siempre funciona bien. Apple incluye forma de desactivarlo",
+"autor" => "Applesfera",
+"resumen" => "iOS 26 introduce el diseño *Liquid Glass*, que hace los elementos del sistema transparentes, pero puede afectar la legibilidad. Apple ya incluye una opción en *Accesibilidad* para reducir la transparencia y mejorar la visibilidad, lo cual es útil mientras la beta se ajusta.
+",
 ];
 ?>
