d2f256ac0a988d529b8db49b35a2a0d0ec05a5b3
Joel Leon M <109642410+JoelLeonUNS@users.noreply.github.com>
2025-06-11 10:16:24 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Builder.ai, el increíble caso de la startup que prometió IA y entregó trabajo manual",
+"autor" => "Entrepreneur",
+"resumen" => "La empresa ocultó el rol de más de 700 ingenieros y simuló operaciones con empresas asociadas, como Microsoft.",
 ];
 ?>
