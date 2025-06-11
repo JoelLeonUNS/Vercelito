c0839e848dac2c7570d4ff3e7160000053e00577
Joel Leon M <109642410+JoelLeonUNS@users.noreply.github.com>
2025-06-11 10:21:21 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Infobae Talks Economía y Finanzas: cómo se adaptan empresas e industrias al nuevo contexto digital",
+"autor" => "Infobae",
+"resumen" => "La próxima edición de *Infobae Talks Economía y Finanzas*, conducida por Agostina Scioli, se centrará en pagos electrónicos, nuevas inversiones y minería como motores del desarrollo argentino. El programa se emitirá en vivo el jueves 19 de junio a las 15 h por la web y el canal de YouTube de Infobae.
+",
 ];
 ?>
